38d93d1c9ebab9d475bed6322745d6ef9430107e
wesleybielen <wesley.bielen@gmail.com>
2024-12-01 23:49:24 +0100

    1st Dec

---
+++ 112_2.py
@@ -0,0 +1,22 @@
+
+
+def main():
+    file_path = "112_input.txt"
+
+    list1 = []
+    list2 = []
+
+    with open(file_path, "r") as file:
+        for line in file:
+            num1, num2 = map(int, line.split())
+            list1.append(num1)
+            list2.append(num2)
+
+    total=0
+    for item in list1:
+        total+= item * list2.count(item)
+
+    print(f"Total: {total}")
+
+if __name__ == "__main__":
+    main()
