17a6f03a92eb65f1f128f73f722f876ba91b35e9
Wesley Bielen <wesley.bielen@aevitae.com>
2024-12-20 18:46:49 +0100

    Dec 15th

--- 1512/1512_2.py
+++ 1512/1512_2.py
@@ -1,51 +1,200 @@
 import os
 import time
-import re
+import copy
 
 def main():
     os.chdir(os.path.dirname(__file__))
-    file_path = "1412_input.txt"
+    file_path = "1512_input_matrix.txt"
 
-    rows=103
-    cols=101
+    with open(file_path, "r") as file:
+        content = file.readlines()
 
+    matrix = [list(line.strip()) for line in content]
+
+    new_matrix=[]
+    #print(matrix)
+
+    file_path = "1512_input_moves.txt"
     with open(file_path, "r") as file:
         content = file.read()
 
-    pattern = r"p=([-]?\d+),\s*([-]?\d+)\s+v=([-]?\d+),\s*([-]?\d+)"
+    moves = list(content)
 
-    matches = re.findall(pattern, content)
-    results = [(tuple(map(int, match[:2])), tuple(map(int, match[2:]))) for match in matches]
+    for i in range(len(matrix)):
+        new_matrix.append([])
+        for ii in range(len(matrix[i])):
+            if matrix[i][ii]=='@':
+                new_matrix[i].append('@')
+                new_matrix[i].append('.')
+            if matrix[i][ii]=='#':
+                new_matrix[i].append('#')
+                new_matrix[i].append('#')
+            if matrix[i][ii]=='.':
+                new_matrix[i].append('.')
+                new_matrix[i].append('.')
+            if matrix[i][ii]=='O':
+                new_matrix[i].append('[')
+                new_matrix[i].append(']')
 
-    i=0
-    while True:
-        new_pos=[]
-        i+=1
-        for p_values, v_values in results:
-            new_place_after_col=None
-            new_place_after_row=None
-            if v_values[0] < 0:
-                new_place_after_col = (p_values[0] + (i * (cols + v_values[0]))) % cols
-            else:
-                new_place_after_col = (p_values[0] + (i * v_values[0])) % cols
+    for line in new_matrix:
+        for char in line:
+                print(char, end=' ') 
+        print()
 
-            if v_values[1] < 0:
-                new_place_after_row = (p_values[1] + (i * (rows + v_values[1]))) % rows
-            else:
-                new_place_after_row = (p_values[1] + (i * v_values[1])) % rows
+    pos=[]
+    for i in range(len(new_matrix)):
+        for ii in range(len(new_matrix[i])):
+            if new_matrix[i][ii]=='@':
+                pos=[i,ii]
+                break
 
-            new_pos.append([new_place_after_row, new_place_after_col])
+    for i in range(len(moves)):
+        move = moves[i]
+        match move:
+            case '<':
+                if pos[1] > 1 and not new_matrix[pos[0]][pos[1]-1] == '#':
+                    if new_matrix[pos[0]][pos[1]-1] == ']':
+                        free_pos = get_next_free_spot((pos[0],pos[1]), new_matrix, '<')
+                        if free_pos:
+                            move_boxes(free_pos, new_matrix, 0, -1, pos)
+                            pos[1]-=1
+                    else:
+                        new_matrix[pos[0]][pos[1]] = '.'
+                        pos[1]-=1
+                        new_matrix[pos[0]][pos[1]] = '@'
+                else:
+                    continue
+            case '>':
+                if pos[1] < (len(new_matrix[0]) -2) and not new_matrix[pos[0]][pos[1] + 1] == '#':
+                    if new_matrix[pos[0]][pos[1] + 1] == '[':
+                        free_pos = get_next_free_spot((pos[0],pos[1]), new_matrix, '>')
+                        if free_pos:
+                            move_boxes(free_pos, new_matrix, 0, 1, pos)
+                            pos[1] += 1
+                    else:
+                        new_matrix[pos[0]][pos[1]] = '.'
+                        pos[1] += 1
+                        new_matrix[pos[0]][pos[1]] = '@'
+                else:
+                    continue
+            case 'v':
+                if pos[0] < (len(new_matrix)-2) and not new_matrix[pos[0]+1][pos[1]] == '#':
+                    if new_matrix[pos[0]+1][pos[1]] in ('[',']'):
+                        free_pos = get_next_free_spot((pos[0],pos[1]), new_matrix, 'v')
+                        if free_pos:
+                            move_boxes(free_pos, new_matrix, 1, 0, pos)
+                            pos[0] += 1
+                    else:
+                        new_matrix[pos[0]][pos[1]] = '.'
+                        pos[0] += 1
+                        new_matrix[pos[0]][pos[1]] = '@'
+                else:
+                    continue
+            case '^':
+                if pos[0] > 1 and not new_matrix[pos[0]-1][pos[1] ] == '#':
+                    if new_matrix[pos[0]-1][pos[1]] in ('[',']'):
+                        free_pos = get_next_free_spot((pos[0],pos[1]), new_matrix, '^')
+                        if free_pos:
+                           move_boxes(free_pos, new_matrix, -1, 0, pos)
+                           pos[0] -= 1
+                    else:
+                        new_matrix[pos[0]][pos[1]] = '.'
+                        pos[0] -= 1
+                        new_matrix[pos[0]][pos[1]] = '@'
+                else:
+                    continue
+    """         print(f'Case after move {move}')
+        for line in new_matrix:
+            for char in line:
+               print(char, end=' ')  # No space
+            print() """
 
-        set_of_tuples = {tuple(lst) for lst in new_pos}
-        if len(set_of_tuples) == len(new_pos):
-            print(f'Found pattern after {i} seconds.')
-            """ xmas_tree= [[' ' for _ in range(cols)] for _ in range(rows)]
-            for pos in new_pos:
-                xmas_tree[pos[0]][pos[1]]='X'
+    total_score=0
+    for i in range(len(new_matrix)):
+        for ii in range(len(new_matrix[0])):
+            if new_matrix[i][ii] == '[':
+                total_score+= (i*100)+ii
+    print(total_score)
 
-            for i in range(len(xmas_tree)):
-                print(' '.join(xmas_tree[i])) """
-            return
+def move_boxes(boxes, matrix, row_jump, col_jump, curr_pos):
+    matrix_temp = copy.deepcopy(matrix)
+    moved_to = set()
+    for pos in boxes:
+        matrix[pos[0]+row_jump][pos[1]+col_jump]=matrix_temp[pos[0]][pos[1]]
+        moved_to.add((pos[0]+row_jump,pos[1]+col_jump))
+
+    for pos in boxes:
+        if not pos in moved_to:
+            matrix[pos[0]][pos[1]]='.'
+
+def get_next_free_spot(pos, matrix, direction) -> list:
+    boxes=set()
+    boxes.add(pos)
+    match direction:
+        case '<':
+            to_explore = boxes.copy()
+            while True:
+                for temp_pos in list(to_explore):
+                    if matrix[temp_pos[0]][temp_pos[1]-1] in ('[',']'):
+                        boxes.add((temp_pos[0], temp_pos[1]-1))
+                        to_explore.add((temp_pos[0], temp_pos[1]-1))
+                    elif matrix[temp_pos[0]][temp_pos[1]-1] == '#':
+                        return set()
+                    to_explore.remove(temp_pos)
+                if not to_explore:
+                    return boxes
+            
+        case '>':
+            to_explore = boxes.copy()
+            while True:
+                for temp_pos in list(to_explore):
+                    if matrix[temp_pos[0]][temp_pos[1]+1] in ('[',']'):
+                        boxes.add((temp_pos[0], temp_pos[1]+1))
+                        to_explore.add((temp_pos[0], temp_pos[1]+1))
+                    elif matrix[temp_pos[0]][temp_pos[1]+1] == '#':
+                        return set()
+                    to_explore.remove(temp_pos) 
+                if not to_explore:
+                    return boxes
+        case 'v':
+            to_explore = boxes.copy()
+            while True:
+                for temp_pos in list(to_explore):
+                    if matrix[temp_pos[0]+1][temp_pos[1]] == '[':
+                        boxes.add((temp_pos[0]+1, temp_pos[1]))
+                        to_explore.add((temp_pos[0]+1, temp_pos[1]))
+                        boxes.add((temp_pos[0]+1, temp_pos[1]+1))
+                        to_explore.add((temp_pos[0]+1, temp_pos[1]+1))
+                    elif matrix[temp_pos[0]+1][temp_pos[1]] == ']':
+                        boxes.add((temp_pos[0]+1, temp_pos[1]))
+                        to_explore.add((temp_pos[0]+1, temp_pos[1]))
+                        boxes.add((temp_pos[0]+1, temp_pos[1]-1))
+                        to_explore.add((temp_pos[0]+1, temp_pos[1]-1))
+                    elif matrix[temp_pos[0]+1][temp_pos[1]] == '#':
+                        return set()
+                    to_explore.remove(temp_pos)
+                if not to_explore:
+                    return boxes
+
+        case '^':
+            to_explore = boxes.copy()
+            while True:
+                for temp_pos in list(to_explore):
+                    if matrix[temp_pos[0]-1][temp_pos[1]] == '[':
+                        boxes.add((temp_pos[0]-1, temp_pos[1]))
+                        to_explore.add((temp_pos[0]-1, temp_pos[1]))
+                        boxes.add((temp_pos[0]-1, temp_pos[1]+1))
+                        to_explore.add((temp_pos[0]-1, temp_pos[1]+1))
+                    elif matrix[temp_pos[0]-1][temp_pos[1]] == ']':
+                        boxes.add((temp_pos[0]-1, temp_pos[1]))
+                        to_explore.add((temp_pos[0]-1, temp_pos[1]))
+                        boxes.add((temp_pos[0]-1, temp_pos[1]-1))
+                        to_explore.add((temp_pos[0]-1, temp_pos[1]-1))
+                    elif matrix[temp_pos[0]-1][temp_pos[1]] == '#':
+                        return set()
+                    to_explore.remove(temp_pos)
+                if not to_explore:
+                    return boxes
 
 if __name__ == "__main__":
     start_time = time.time()
